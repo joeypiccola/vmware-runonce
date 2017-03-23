0482c15618085bea860e2bfa540c1e8b8e1cbb96
joeypiccola <joey.piccola@gmail.com>
2017-03-23 15:41:42 -0600

    changed deployment detection logic

--- ms.ps1
+++ ms.ps1
@@ -9,7 +9,9 @@ New-MyFolder
 Set-FinRunOnce
 Stop-Transcript
 # reboot
-if ((Get-WmiObject -Class win32_computersystem).Manufacturer -eq 'VMware, Inc.')
+#if ((Get-WmiObject -Class win32_computersystem).Manufacturer -eq 'VMware, Inc.')
+# if this folder exists then assume MDT or SCCM built the server. Let the TS handle the reboot and auto logon (i.e. LogonCount)
+if (!(Test-Path -Path C:\MININT))
 {
     shutdown /r /t 60 /c "reboot post deployment"
 }
