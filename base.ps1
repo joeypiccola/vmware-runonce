10c7acfde159234dfe3aa740cff74ece7f18b36f
joeypiccola <joey.piccola@gmail.com>
2017-11-13 21:51:44 -0700

    added offline disks

--- base.ps1
+++ base.ps1
@@ -89,3 +89,111 @@ function Update-Networking
     # register dns
     ipconfig /registerdns
 }
+
+Function Configure-OfflineDisks
+{ 
+ 
+    #Check for offline disks on server. 
+    $offlinedisk = "list disk" | diskpart | where {$_ -match "offline"} 
+     
+    #If offline disk(s) exist 
+    if($offlinedisk.count -eq 1) 
+    { 
+     
+        Write-Output "Following Offline disk(s) found..Trying to bring Online." 
+        $offlinedisk 
+         
+        #for all offline disk(s) found on the server 
+        foreach($offdisk in $offlinedisk) 
+        { 
+     
+            $offdiskS = $offdisk.Substring(2,6) 
+            Write-Output "Enabling $offdiskS" 
+#Creating command parameters for selecting disk, making disk online and setting off the read-only flag. 
+$OnlineDisk = @" 
+select $offdiskS
+attributes disk clear readonly
+online disk
+select $offdiskS
+clean
+convert gpt
+create partition primary
+format quick fs=ntfs label="Data" unit=4096
+assign letter="D"
+"@ 
+            #Sending parameters to diskpart 
+            $noOut = $OnlineDisk | diskpart 
+            sleep 5 
+     
+       } 
+ 
+        #If selfhealing failed throw the alert. 
+        if(($offlinedisk = "list disk" | diskpart | where {$_ -match "offline"} )) 
+        { 
+         
+            Write-Output "Failed to bring the following disk(s) online" 
+            $offlinedisk 
+ 
+        } 
+        else 
+        { 
+     
+            Write-Output "Disk(s) are now online." 
+ 
+        } 
+ 
+    }
+    elseif ($offlinedisk.count -gt 1)
+    {
+        Write-Output "Following Offline disk(s) found..Trying to bring Online." 
+        $offlinedisk 
+         
+        #for all offline disk(s) found on the server 
+        foreach($offdisk in $offlinedisk) 
+        { 
+     
+            $offdiskS = $offdisk.Substring(2,6) 
+            Write-Output "Enabling $offdiskS" 
+#Creating command parameters for selecting disk, making disk online and setting off the read-only flag. 
+$OnlineDisk = @" 
+select $offdiskS
+attributes disk clear readonly
+online disk
+select $offdiskS
+clean
+convert gpt
+create partition primary
+format quick fs=ntfs label="Data" unit=4096
+"@ 
+            #Sending parameters to diskpart 
+            $noOut = $OnlineDisk | diskpart 
+            sleep 5 
+     
+       } 
+ 
+        #If selfhealing failed throw the alert. 
+        if(($offlinedisk = "list disk" | diskpart | where {$_ -match "offline"} )) 
+        { 
+         
+            Write-Output "Failed to bring the following disk(s) online" 
+            $offlinedisk 
+ 
+        } 
+        else 
+        { 
+     
+            Write-Output "Disk(s) are now online." 
+ 
+        }    
+    
+    } 
+ 
+    #If no offline disk(s) exist. 
+    else 
+    { 
+ 
+        #All disk(s) are online. 
+        Write-Host "All disk(s) are online!" 
+ 
+    } 
+}
