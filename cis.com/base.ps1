25f4b0afe5803e0818456148be25839348e5f366
joeypiccola <joey.piccola@gmail.com>
2018-03-05 11:35:40 -0700

    refactor

---
+++ cis.com/base.ps1
@@ -0,0 +1,15 @@
+﻿Start-Transcript  -Path C:\deploy\vmwaredeployment.txt -Append -Force
+. 'c:\deploy\config\base.ps1'
+
+# base function calls
+Rename-CDROM
+New-MyFolder
+Set-KMS
+#Install-BaseModules
+Configure-OfflineDisks
+
+# clean up
+Set-FinRunOnce
+Stop-Transcript
+# reboot
+Reboot-System
